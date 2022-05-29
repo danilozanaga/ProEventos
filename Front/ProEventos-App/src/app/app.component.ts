af210af6dc40a6042924d3f1ed8cd1c45e971695
danilozanaga <danilozanaga@gmail.com>
2022-05-29 17:04:18 -0300

    dashboard

--- Front/ProEventos-App/src/app/app.component.ts
+++ Front/ProEventos-App/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ProEventos-App';
-}
+export class AppComponent{ }
